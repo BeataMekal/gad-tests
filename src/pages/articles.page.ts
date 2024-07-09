cb240dbcce6bd25434c35d91d89a8561270994e1
Beata Lapek <beatakuzlecka@o2.pl>
2024-07-09 21:57:31 +0200

    test:main menu smokes

--- src/pages/articles.page.ts
+++ src/pages/articles.page.ts
@@ -1,8 +1,10 @@
+import { MainMenuComponents } from '../components/main-menu.components';
 import { BasePage } from './base.page';
 import { Page } from '@playwright/test';
 
 export class ArticlesPage extends BasePage {
   url = '/articles.html';
+  mainMenu = new MainMenuComponents(this.page);
 
   constructor(page: Page) {
     super(page);
--- src/pages/comments.page.ts
+++ src/pages/comments.page.ts
@@ -1,8 +1,11 @@
+import { MainMenuComponents } from '../components/main-menu.components';
 import { BasePage } from './base.page';
 import { Page } from '@playwright/test';
 
 export class CommentsPage extends BasePage {
   url = '/comments.html';
+  mainMenu = new MainMenuComponents(this.page);
+
   constructor(page: Page) {
     super(page);
   }
