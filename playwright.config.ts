7bdbe58ac56b11aff555b5c9cbabe4e90a0addfd
Beata Lapek <beatakuzlecka@o2.pl>
2024-07-10 21:47:55 +0200

    feat: dotenv for env var

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  globalSetup: 'src/global-setup.ts',
   timeout: 60_000,
   expect: { timeout: 10_000 },
   fullyParallel: true,
@@ -12,10 +13,10 @@ export default defineConfig({
   workers: undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL,
     actionTimeout: 0,
     trace: 'on',
-    video: 'on',
+    video: 'retain-on-failure',
     screenshot: 'only-on-failure',
   },
 
