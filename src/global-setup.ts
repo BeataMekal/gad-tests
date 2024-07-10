51e4bb0688f8dadc45414b7b51088a6eb574e295
Beata Lapek <beatakuzlecka@o2.pl>
2024-07-10 22:20:46 +0200

    chore:global-setup console log commented

--- src/global-setup.ts
+++ src/global-setup.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 async function globalSetup(): Promise<void> {
   dotenv.config({ override: true }); //to overwrite the environment variable from the system with the env file
-  console.log('⚠️  URL:', process.env.BASE_URL);
+  // console.log('⚠️  URL:', process.env.BASE_URL);
 }
 
 export default globalSetup;
