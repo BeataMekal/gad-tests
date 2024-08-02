c43222c6b68a54a555dd85fd9e19ab263b8db89c
Beata Lapek <beatakuzlecka@o2.pl>
2024-08-02 20:29:19 +0200

    chore: typescript error corrected

--- src/global-setup.ts
+++ src/global-setup.ts
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 
 async function globalSetup(): Promise<void> {
   dotenv.config({ override: true }); //to overwrite the environment variable from the system with the env file
