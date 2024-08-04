d4208f4562c7c2232574d046ebe5655f25e2b70c
Beata Lapek <beatakuzlecka@o2.pl>
2024-08-04 20:01:55 +0200

    wpi: comment lifecycle, verify comment

--- src/pages/article.page.ts
+++ src/pages/article.page.ts
@@ -1,7 +1,11 @@
 import { MainMenuComponents } from '../components/main-menu.components';
 import { BasePage } from './base.page';
-import { Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 
+interface ArticleComment {
+  body: Locator;
+  link: Locator;
+}
 export class ArticlePage extends BasePage {
   url = '/article.html';
   mainMenu = new MainMenuComponents(this.page);
@@ -28,5 +32,16 @@ export class ArticlePage extends BasePage {
   async searchArticle(phrase: string): Promise<void> {
     await this.searchInput.fill(phrase);
     await this.goSearchButton.click();
+  }
+
+  getArticleComment(body: string): ArticleComment {
+    const commentContainer = this.page
+      .locator('.comment-container')
+      .filter({ hasText: body });
+
+    return {
+      body: commentContainer.locator(':text("comment:") + span'),
+      link: commentContainer.locator("[id^='gotoComment']"),
+    };
   }
 }
