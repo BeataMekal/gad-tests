168d8f1421363affe897cb6bde9a95cceed9e43c
Beata Lapek <beatakuzlecka@o2.pl>
2024-09-17 22:23:12 +0200

    chore: refactor comments views to return po

--- tests/end-to-end/comment-lifecycle.spec.ts
+++ tests/end-to-end/comment-lifecycle.spec.ts
@@ -37,13 +37,13 @@ test.describe('Create, verify and delete comment', () => {
         await expect
           .soft(addCommentView.addNewHeader)
           .toHaveText(expectedAddCommentHeader);
-        await addCommentView.createComment(newCommentData);
+        articlePage = await addCommentView.createComment(newCommentData);
         //Assert
         await expect
           .soft(articlePage.alertPopup)
           .toHaveText(expectedCommentCreatedPopup);
       });
-      const commentPage = await test.step('verify comment', async () => {
+      let commentPage = await test.step('verify comment', async () => {
         //Act
         const articleComment = articlePage.getArticleComment(
           newCommentData.body,
@@ -66,7 +66,7 @@ test.describe('Create, verify and delete comment', () => {
 
         //Act
         const editCommentView = await commentPage.clickEditButton();
-        await editCommentView.updateComment(editCommentData);
+        commentPage = await editCommentView.updateComment(editCommentData);
         //Assert
         await expect
           .soft(commentPage.alertPopup)
@@ -98,7 +98,7 @@ test.describe('Create, verify and delete comment', () => {
 
         //Act
         const addCommentView = await articlePage.clickAddCommentButton();
-        await addCommentView.createComment(newCommentData);
+        articlePage = await addCommentView.createComment(newCommentData);
         //Assert
         await expect
           .soft(articlePage.alertPopup)
@@ -110,7 +110,7 @@ test.describe('Create, verify and delete comment', () => {
             const secondCommentData = prepareRandomComment();
 
             const addCommentView = await articlePage.clickAddCommentButton();
-            await addCommentView.createComment(secondCommentData);
+            articlePage = await addCommentView.createComment(secondCommentData);
             return secondCommentData.body;
           });
         await test.step('verify comment', async () => {
