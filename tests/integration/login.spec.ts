af995c578ab6287aae1ed659c8d8bfa2f939917c
Beata Lapek <beatakuzlecka@o2.pl>
2024-11-19 21:13:58 +0100

    chore: own fixtures in login tests

--- tests/integration/login.spec.ts
+++ tests/integration/login.spec.ts
@@ -1,19 +1,14 @@
+import { expect, test } from '@_src/fixtures/merge.fixture';
 import { LoginUserModel } from '@_src/models/user.model';
-import { LoginPage } from '@_src/pages/login.page';
 import { testUser1 } from '@_src/test-data/user-data';
-import { expect, test } from '@playwright/test';
 
 test.describe('Verify login', () => {
   test(
     'login with correct credentials',
     { tag: '@GAD-R02-01' },
-    async ({ page }) => {
+    async ({ loginPage }) => {
       //Arrange
       const expectedWelcomeTitle = 'Welcome';
-      const loginPage = new LoginPage(page);
-
-      //Act
-      await loginPage.goto();
       const welcomePage = await loginPage.login(testUser1);
 
       const title = await welcomePage.getTitle();
@@ -25,17 +20,15 @@ test.describe('Verify login', () => {
   test(
     'reject login with incorrect password',
     { tag: '@GAD-R02-01' },
-    async ({ page }) => {
+    async ({ loginPage }) => {
       //Arrange
       const expectedLoginTitle = 'Login';
-      const loginPage = new LoginPage(page);
       const loginUserData: LoginUserModel = {
         userEmail: testUser1.userEmail,
         userPassword: 'incorrectPassword',
       };
 
       //Act
-      await loginPage.goto();
       await loginPage.login(loginUserData);
 
       //Assert
