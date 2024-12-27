360b164c7b99962833b14ed9b345304c9346d1c9
Beata Lapek <beatakuzlecka@o2.pl>
2024-12-27 22:27:49 +0100

    test: go button action

---
+++ tests/integration/search.spec.ts
@@ -0,0 +1,21 @@
+import { expect, test } from '@_src/fixtures/merge.fixture';
+
+test.describe('Verify search component for articles', () => {
+  test(
+    'go button should fetch articles',
+    { tag: '@GAD-R07-01' },
+    async ({ articlesPage, page }) => {
+      //Arrange
+      const expectDefaultArticleNumber = 6;
+      await expect(articlesPage.goSearchButton).toBeInViewport();
+      const responsePromise = page.waitForResponse('/api/articles*');
+      //Act
+      await articlesPage.goSearchButton.click();
+      const response = await responsePromise;
+      const body = await response.json();
+      //Assert
+      expect(response.ok()).toBeTruthy(); //sprawdza czy odpowiedź jest pozytywna
+      expect(body).toHaveLength(expectDefaultArticleNumber);
+    },
+  );
+});
